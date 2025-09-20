e08739092dba56aebeefa225bb40347b5010463e
halil gezer <hg_1993@windowslive.com>
2025-09-20 08:14:18 +0300

    test ve manuel fatura eklendi

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "32c39f130c4e05d344e47d27b233c4ea
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "e2e4790416134e45aef037b96c56fee9",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "3e2e3f8e5134f2f7e382a37eddf1cbc8",
+"flutter_bootstrap.js": "db58ce7c7e68eccff0b733ecd29c7cd5",
 "icons/Icon-192.png": "96cc2a42a592230c4d07391004b2f98f",
 "icons/Icon-512.png": "7515de3df657e173e4ef3dbfe33f2341",
 "icons/Icon-maskable-192.png": "96cc2a42a592230c4d07391004b2f98f",
 "icons/Icon-maskable-512.png": "7515de3df657e173e4ef3dbfe33f2341",
 "index.html": "7800bfa4ac595a1ea4f999b5a14c8a61",
 "/": "7800bfa4ac595a1ea4f999b5a14c8a61",
-"main.dart.js": "95c0ea5b623f993ef46cfe593053de8f",
+"main.dart.js": "8adde2409ef2083e1c3e6d36b16a75b1",
 "manifest.json": "3a075b1a4f83c1e644ba73b6b23d47d9",
 "version.json": "7fd4bc9327963243febd2ef3a831aabc"};
 // The application shell files that are downloaded before a service worker can
